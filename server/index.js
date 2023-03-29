69935b3139e251903f263c4f120b4a1f721e07e8
sravan <sravanpthankappan@gmail.com>
2023-03-29 15:42:32 +0530

    route changed

--- server/index.js
+++ server/index.js
@@ -4,35 +4,44 @@ const app = express()
 const cors = require('cors')
 const fs = require('fs')
 const csv = require('fast-csv')
-const data = []
 app.use(express.json())
 
 app.use(cors())
 
-fs.createReadStream('./assets/AAPL.csv')
-  .pipe(csv.parse({ headers: true }))
-  .on('error', error => console.error(error))
-  .on('data', row => data.push(row))
-  .on('end', () => console.log(data));
- 
 
-app.get('/details',async(req,res)=>{
-      
+
+
+
+app.get('/', (req, res) => {
+
     try {
-
-        const details = await data
-        res.status(200).json(data)
         
+        const data = []
+        fs.createReadStream('./assets/AAPL.csv')
+
+            .pipe(csv.parse({ headers: true }))
+
+            .on('error', error => {
+                throw new Error(error)
+            })
+            .on('data', row => data.push(row))
+
+            .on('end', () => {
+                res.status(200).json(data)
+            });
+            
     } catch (error) {
-         
-        res.status(500).json(error)
+       
+          res.status(500).json(error)
     }
     
-    
+
+
+
 })
 
 
-app.listen(5000,console.log("server started"))
+app.listen(5000, console.log("server started"))
 
 
 
